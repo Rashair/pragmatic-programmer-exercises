a1a6a86725fc04d1ce2470ea12059d91a9c67e2d
Rashair <przemyslaw.wk@gmail.com>
2021-09-18 18:27:21 +0200

    Add camelCase-scanner exercise solution

--- text-manipulation/camelCase-scanner/camelCase-scanner.py
+++ text-manipulation/camelCase-scanner/camelCase-scanner.py
@@ -1,6 +1,20 @@
+import re
 from glob import glob
 
 dir_in = "tests"
 
 for file in glob(dir_in + '/*.java'):
-    print(file)
+    print("\n" + file)
+    with open(file) as file_in:
+        line_num = 0
+        for line in file_in:
+            line_num += 1
+            matches = re.findall(" ([$a-z0-9]+[A-Z][a-zA-Z0-9$]*)", line)
+            if matches == []:
+                continue
+
+            words = "'" + ', '.join(matches) + "'"
+            print(
+                f"Camel-case words detected in line {line_num}:   {words}")
+            print(f"Line: '{line.strip()}'")
+            print()
