cba733c612fb9947ee9746d2b5b650cb1c532b00
Rashair <przemyslaw.wk@gmail.com>
2021-11-06 00:44:22 +0100

    Add initial implementation of string-reader state machine

---
+++ state-machine/StateMachine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace StateMachine
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Provide input file: ");
+            var path = Console.ReadLine();
+
+            var machine = new StateMachine();
+            var strings = machine.Execute(new StreamReader(File.OpenRead(path)));
+
+            Console.WriteLine("Strings:");
+            foreach(var str in strings)
+            {
+                Console.WriteLine($@"""{str}""");
+            }
+        }
+    }
+}
---
+++ state-machine/StateMachine/StateMachine.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachine
+{
+    class StateMachine
+    {
+        private readonly List<string> strings;
+        private State _currentState;
+        private StringBuilder currentStringBuilder;
+
+        public StateMachine()
+        {
+            strings = new();
+            _currentState = State.OUTSIDE_STRING;
+        }
+
+        public List<string> Execute(StreamReader reader)
+        {
+            while (!reader.EndOfStream)
+            {
+                var c = (char)reader.Read();
+                Process(c);
+            }
+            return strings;
+        }
+
+        private void Process(char c)
+        {
+            _currentState = _currentState switch
+            {
+                State.OUTSIDE_STRING => OutsideString(c),
+                State.STRING_START => StringStart(c),
+                State.IN_STRING => InString(c),
+                State.STRING_END => StringEnd(c),
+                _ => throw new InvalidOperationException(),
+            };
+        }
+
+        private State InString(char c)
+        {
+            switch (c)
+            {
+                case '"':
+                    return State.STRING_END;
+
+                default:
+                    currentStringBuilder.Append(c);
+                    return State.IN_STRING;
+            }
+        }
+
+        public State StringStart(char c)
+        {
+            switch (c)
+            {
+                case '"':
+                    return State.STRING_END;
+
+                default:
+                    currentStringBuilder = new(c.ToString());
+                    return State.IN_STRING;
+            }
+        }
+
+        public State OutsideString(char c)
+        {
+            switch (c)
+            {
+                case '"':
+                    return State.STRING_START;
+
+                default:
+                    return State.OUTSIDE_STRING;
+            }
+        }
+
+        public State StringEnd(char c)
+        {
+            strings.Add(currentStringBuilder.ToString());
+            currentStringBuilder = null;
+
+            switch (c)
+            {
+                case '"':
+                    return State.STRING_START;
+
+                default:
+                    currentStringBuilder = new();
+                    return State.OUTSIDE_STRING;
+            }
+        }
+    }
+
+    internal enum State
+    {
+        OUTSIDE_STRING,
+        STRING_START,
+        IN_STRING,
+        STRING_END,
+    }
+}
